f732e1d4241a391297c55f029d520a80b1990433
sarojmaharana1980 <maharana.saroj2020@gmail.com>
2021-12-18 23:21:58 +0530

    Added javascript function to display nested value

---
+++ Challenge-3/nested.js
@@ -0,0 +1,23 @@
+var object, nested;
+
+//object = {“a”: {“b”: {“c”: ”d”}}};
+//key = "a/b/c"
+
+
+
+nested = function(object, key) {
+	  var pairs;
+	  //get pair length
+	  if (!(pairs = _(object).pairs()).length) {
+	    return [
+	      {
+	        keys: key,
+	        value: object
+	      }
+	    ];
+	  } else {
+	    return [].concat.apply([], _(pairs).map(function(kv) {
+	      return nested(kv[1], key.concat(kv[0]));
+	    }));
+	  }
+	};
